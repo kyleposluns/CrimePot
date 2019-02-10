3df8198e99612d2f6409ec1fa2ff00f4b5993a94
Kyle Posluns <kyleposluns@gmail.com>
2019-02-10 00:59:54 -0500

    Changed radius to use miles instead of kilometers.

--- geojsoncompat.py
+++ geojsoncompat.py
@@ -10,7 +10,7 @@ def distBetween(lon1, lat1, lon2, lat2):
   dlat = lat2 - lat1
   a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
   c = 2 * asin(sqrt(a))
-  r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+  r = 3956  # Radius of earth in kilometers. Use 6371 for kilometers.
   return(c * r)
 
 def filterByRadius(dict, lat, long, radius):
