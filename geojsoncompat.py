d38079d45e37d8ac826add980c28889649892c2a
Kyle Posluns <kyleposluns@gmail.com>
2019-02-09 23:33:33 -0500

    "Fixed all bugs. Correct data is now pushed as GeoJSON.

--- geojsoncompat.py
+++ geojsoncompat.py
@@ -1,5 +1,6 @@
 import json
 from math import radians, cos, sin, asin, sqrt
+import string
 
 
 def distBetween(lon1, lat1, lon2, lat2):
@@ -7,7 +8,7 @@ def distBetween(lon1, lat1, lon2, lat2):
   # haversine formula
   dlon = lon2 - lon1
   dlat = lat2 - lat1
-  a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+  a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
   c = 2 * asin(sqrt(a))
   r = 6371  # Radius of earth in kilometers. Use 3956 for miles
   return(c * r)
@@ -48,7 +49,7 @@ def filterByRadius(dict, lat, long, radius):
               "Date": month + "/" + date[8:10] + "/" + year,
               "Time": date[11:],
               "District": object["district"],
-              "Street": object["street"]
+              "Street": object["street"].title()
           }
       })
       geoJsonDicts["features:"] = arrayOfObjects
